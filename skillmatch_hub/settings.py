70a1f18cebb9a19b130f9e0e6c1a09d7a5ebdbbd
Radzi <r.zradzizamri@gmail.com>
2025-12-24 20:25:47 +0800

    Configure for Render deployment

--- skillmatch_hub/settings.py
+++ skillmatch_hub/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '7_88yoat+b%2ofcts9$t42iak@0yiv2h5@7nq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = []
+if 'RENDER_EXTERNAL_HOSTNAME' in os.environ:
+    ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
+if 'ALLOWED_HOSTS' in os.environ:
+    ALLOWED_HOSTS.extend(os.environ['ALLOWED_HOSTS'].split(','))
+if not ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
